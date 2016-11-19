27db20999bc11509754b2c87f51211e5018e1b46
Koen van Gilst <koen.vangilst@gmail.com>
2016-11-19 21:14:50 +0100

    add wrapper for Question-Cards

--- src/QuestionCard.js
+++ src/QuestionCard.js
@@ -10,18 +10,20 @@ const showError = (correctAnswer, userAnswer) => (
 class QuestionCard extends Component {
   render () {
     return (
-      <Paper className='Question-Card' zDepth={1}>
-        <p>
-          Shortcut to
-          <code> {this.props.question.q} </code>
-          in memoQ?
-        </p>
-        <TextField
-          id='user-answer'
-          errorText={showError(this.props.question.a, this.props.userAnswer)}
-          value={this.props.userAnswer} />
-        <div>{(this.props.question.a === this.props.userAnswer) ? ' 👍' : ' 🤓'}</div>
-      </Paper>
+      <div className='Question-Wrapper'>
+        <Paper className='Question-Card' zDepth={1}>
+          <p>
+            Shortcut to
+            <code> {this.props.question.q} </code>
+            in memoQ?
+          </p>
+          <TextField
+            id='user-answer'
+            errorText={showError(this.props.question.a, this.props.userAnswer)}
+            value={this.props.userAnswer} />
+          <div>{(this.props.question.a === this.props.userAnswer) ? ' 👍' : ' 🤓'}</div>
+        </Paper>
+      </div>
     )
   }
 }

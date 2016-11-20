2794f5df1138b66923ab306996e8308dab3f2fd1
Koen van Gilst <koen.vangilst@gmail.com>
2016-11-20 20:27:52 +0100

    fixes tests by adding Root component

---
+++ src/Root.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import App from './App'
+
+const Root = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+)
+
+export default Root
--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import App from './App'
+import Root from './Root.js'
 import './index.css'
-
-const Root = () => (
-  <MuiThemeProvider>
-    <App />
-  </MuiThemeProvider>
-)
 
 ReactDOM.render(
   <Root />,

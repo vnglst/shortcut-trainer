d37cf172f149d310d065361a4c86e09d3955ab1a
Koen van Gilst <koen.vangilst@gmail.com>
2016-11-24 11:44:15 +0100

    reverses question history

--- src/containers/VisibleHistory.js
+++ src/containers/VisibleHistory.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux'
 import QuestionHistory from '../components/question/QuestionHistory'
 
 const mapStateToProps = (state) => ({
-  questions: state.questions.list.filter(q => q.completed)
+  questions: state.questions.list.filter(q => q.completed).reverse()
 })
 
 const VisibleHistory = connect(

82d508ce8ba6424a23dbdcb08cf37907ef400a16
Koen van Gilst <koen.vangilst@gmail.com>
2016-11-26 21:23:58 +0100

    refactor Question component as funtional component

--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import TextField from 'material-ui/TextField'
 import ShowQuestion from './ShowQuestion'
 
@@ -12,46 +12,34 @@ const isModifier = (key) => {
   return modifiers.indexOf(key) !== -1
 }
 
-class Question extends Component {
-
-  constructor (props) {
-    super(props)
-    this.dispatch = this.props.dispatch
-    this.handleKeypress = this.handleKeypress.bind(this)
-    this.handleKeyRelease = this.handleKeyRelease.bind(this)
+const Question = ({dispatch, currentQuestion, userAnswer, addKey, removeKey, answerQuestion, reset}) => {
+  const handleKeypress = (e) => {
+    e.preventDefault()
+    addKey(e.key)
   }
 
-  handleKeypress (e) {
+  const handleKeyRelease = (e) => {
     e.preventDefault()
-    this.props.addKey(e.key)
-  }
-
-  handleKeyRelease (e) {
-    e.preventDefault()
-    const { currentQuestion, userAnswer, removeKey, answerQuestion } = this.props
     if (!isModifier(e.key)) {
       answerQuestion(currentQuestion.id, userAnswer)
     }
     removeKey(e.key)
   }
 
-  render () {
-    const { currentQuestion, userAnswer, reset } = this.props
-    const questionString = "What's the keyboard shortcut for:"
-    return (
-      <div className='Question'>
-        <p>{questionString}</p>
-        <TextField
-          id='user-answer'
-          autoFocus
-          floatingLabelText={currentQuestion.q}
-          value={userAnswer}
-          onKeyDown={this.handleKeypress}
-          onKeyUp={this.handleKeyRelease}
-          onBlur={reset} />
-      </div>
-    )
-  }
+  const questionString = "What's the keyboard shortcut for:"
+  return (
+    <div>
+      <p>{questionString}</p>
+      <TextField
+        id='user-answer'
+        autoFocus
+        floatingLabelText={currentQuestion.q}
+        value={userAnswer}
+        onKeyDown={handleKeypress}
+        onKeyUp={handleKeyRelease}
+        onBlur={reset} />
+    </div>
+  )
 }
 
 export default ShowQuestion(Question)

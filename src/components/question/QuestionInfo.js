5b5d4fed1f0c0e58d49e43a877a894b0fc55f4cf
Koen van Gilst <koen.vangilst@gmail.com>
2016-11-27 20:19:22 +0100

    refactor using functional components

--- src/components/question/QuestionInfo.js
+++ src/components/question/QuestionInfo.js
@@ -1,26 +1,23 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {ListItem} from 'material-ui/List'
 import ThumbUp from 'material-ui/svg-icons/action/thumb-up'
 import ThumbDown from 'material-ui/svg-icons/action/thumb-down'
 import {red500, greenA200} from 'material-ui/styles/colors'
 import './QuestionInfo.css'
 
-class QuestionInfo extends Component {
-  render () {
-    const { question } = this.props
-    const correctAnswer = question.a === question.userAnswer
-    let info = <span>{question.q + ` - `}
-      <span className={correctAnswer ? 'right' : 'wrong'}>
-        {question.userAnswer}
-      </span>
-      <span className='right'>
-        {correctAnswer ? '' : ' - ' + question.a}
-      </span>
+const QuestionInfo = ({question}) => {
+  const correctAnswer = question.a === question.userAnswer
+  let info = <span>{question.q + ` - `}
+    <span className={correctAnswer ? 'right' : 'wrong'}>
+      {question.userAnswer}
     </span>
-    return (
-      <ListItem disabled primaryText={info} rightIcon={correctAnswer ? <ThumbUp color={greenA200} /> : <ThumbDown color={red500} />} />
-    )
-  }
+    <span className='right'>
+      {correctAnswer ? '' : ' - ' + question.a}
+    </span>
+  </span>
+  return (
+    <ListItem disabled primaryText={info} rightIcon={correctAnswer ? <ThumbUp color={greenA200} /> : <ThumbDown color={red500} />} />
+  )
 }
 
 export default QuestionInfo
